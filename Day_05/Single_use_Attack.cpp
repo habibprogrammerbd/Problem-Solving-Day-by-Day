752532683d44a612836bb012221ecd411e761b5b
Ahosan Habib Programmer <habibprogrammerbd@gmail.com>
2026-07-11 17:23:58 +0600

    day_05_1

--- Day_05/Single_use_Attack.cpp
+++ Day_05/Single_use_Attack.cpp
@@ -1,11 +1,28 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
-    int a,x,y;
-    cin >> a >> x >> y;
+    int t;
+    cin >> t;
 
-    
+    while (t--)
+    {
+        int a, x, y;
+        cin >> a >> x >> y;
+
+        int ans1 = a / x;
+        int ans2 = a - y;
+
+        if (a % y == 0)
+        {
+            cout << ans2 +1 << endl;
+        }    
+        else
+        {
+            cout << ans1 << endl;
+        }
+    }
+
     return 0;
 }
